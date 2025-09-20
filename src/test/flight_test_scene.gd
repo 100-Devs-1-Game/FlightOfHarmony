98f1b0715ebaddc6b0d93a759f370335bbbd4d7c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-20 22:44:55 +0200

    Add clouds

--- src/test/flight_test_scene.gd
+++ src/test/flight_test_scene.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+@export var num_clouds: int= 100
+@export var cloud_step: int= 50
+@export var max_cloud_height: int= 5000
+
+
+func _draw() -> void:
+	var color:= Color.WHITE
+	color.a= 0.5
+	
+	for i in num_clouds:
+		draw_rect(Rect2(i * cloud_step, randf_range(-max_cloud_height, 500), 100, 30), color)
