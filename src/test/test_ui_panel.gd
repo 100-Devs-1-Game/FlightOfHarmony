1a737682abee916f52193a659c7e96f7aee7c0d8
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-20 22:33:21 +0200

    Update panel

--- src/test/test_ui_panel.gd
+++ src/test/test_ui_panel.gd
@@ -2,9 +2,16 @@ extends PanelContainer
 
 signal reset_pony
 
+@export var pony: FlyingPony
 @export var stat_panel_scene: PackedScene
+
+@onready var label_distance: Label = %"Label Distance"
 
 
 
 func _on_button_reset_pressed() -> void:
 	reset_pony.emit()
+
+
+func _physics_process(delta: float) -> void:
+	label_distance.text= "Distance: %d" % int(pony.position.x / 10)
