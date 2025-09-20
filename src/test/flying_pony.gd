4856ecef26ba085e12af85db2d4728fb535c8767
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-20 22:40:04 +0200

    Fix drag

--- src/test/flying_pony.gd
+++ src/test/flying_pony.gd
@@ -69,7 +69,7 @@ func fly_logic(delta: float):
 
 func get_drag()-> float:
 	var optimal_drag: float= stats.get_stat_value(PonyStats.StatEnum.DRAG)
-	var dot: float= global_transform.x.dot(velocity.normalized())
+	var dot: float= global_transform.x.dot(Vector2.RIGHT)
 	return lerp(maximum_drag, optimal_drag, clampf(dot, 0.0, 1.0))
 
 
