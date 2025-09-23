5fb469f0d5327f4d22c629a4f75a0390ce110dc6
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-23 10:38:50 +0200

    Fix warnings

--- src/flight/follow_camera.gd
+++ src/flight/follow_camera.gd
@@ -5,6 +5,6 @@ extends Camera2D
 
 
 ## follow the pony but not for pony.position.x < 0
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	position= follow_node.position
-	position.x= max(1920 / 2, position.x)
+	position.x= max(1920 / 2.0, position.x)
