4d5a29228db0862c2af145e31f5b18d2a7c09ee2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-25 02:13:35 +0200

    Try to remove custom scripts early

--- src/tools/upgrade_overlay_tweaking.gd
+++ src/tools/upgrade_overlay_tweaking.gd
@@ -4,4 +4,12 @@ extends Node2D
 
 
 func _ready() -> void:
+	# Remove potential custom scripts from upgrade scenes to be tested
+	# Because those scripts expect a flying pony reference
+	# .. but anything that runs in `_ready()` in those scripts will be
+	# executed earlier than this code, so it's not perfect
+	for child in get_children():
+		if child is UpgradeOverlayScene:
+			child.set_script(null)
+			
 	animated_sprite_flying_pony.play("flight")
