b951cd993ac2c4404f188db88b8f7205f24ae1d8
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-28 18:03:09 +0200

    Added some more descriptions

--- src/core/pony_stat_modifier.gd
+++ src/core/pony_stat_modifier.gd
@@ -2,6 +2,17 @@ class_name PonyStatModifier
 extends Resource
 ## Used to describe the value of a single stat changed by
 ## a PonyUpgrade
+## For example a Glider that adds level 3 lift:
+## ( Lift, 3 ) => should show a Bar called "Lift" with 3 bars
+## in the Upgrade Book
 
+
+## Which flying pony stat are we modifying
 @export var stat: PonyStats.StatType
+## The level of this stat ( 0 - 10 )
 @export var value: int
+
+
+
+func get_display_name()-> String:
+	return PonyStats.StatType.keys()[stat]
