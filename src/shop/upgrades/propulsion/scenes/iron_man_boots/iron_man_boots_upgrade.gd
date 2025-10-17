92f15c7ee3447e0e943ce776fbbeee7f52ad2b2d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-17 11:20:59 +0200

    Fix animation

--- src/shop/upgrades/propulsion/scenes/iron_man_boots/iron_man_boots_upgrade.gd
+++ src/shop/upgrades/propulsion/scenes/iron_man_boots/iron_man_boots_upgrade.gd
@@ -5,7 +5,11 @@ extends UpgradeOverlayScene
 
 
 func _ready() -> void:
+	animated_sprite.hide()
+	
 	pony.start_propulsion.connect(func():
+		animated_sprite.show()
 		animated_sprite.play("default"))
 	pony.stop_propulsion.connect(func():
+		animated_sprite.hide()
 		animated_sprite.stop())
