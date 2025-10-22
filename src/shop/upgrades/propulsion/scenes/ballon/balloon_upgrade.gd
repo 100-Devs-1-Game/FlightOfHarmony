939e4e76c74ca5ec1c75b957e613966c69c0c74b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-22 12:33:41 +0200

    Dynamic balloon sprite scaling

---
+++ src/shop/upgrades/propulsion/scenes/ballon/balloon_upgrade.gd
@@ -0,0 +1,10 @@
+extends UpgradeOverlayScene
+
+@onready var sprite: Sprite2D = $Sprite2D
+
+
+
+
+func _physics_process(delta: float) -> void:
+	var fuel_ratio: float= pony.get_fuel_ratio()
+	sprite.scale= lerp(Vector2.ONE * 0.3, Vector2.ONE, fuel_ratio) 
