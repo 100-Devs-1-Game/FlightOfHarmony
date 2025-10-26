0f2baf7e0551a112a468996cc23a29c3cf8248b4
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-26 06:33:06 +0100

    Start menu update

--- src/start_menu/start_menu.gd
+++ src/start_menu/start_menu.gd
@@ -6,33 +6,17 @@ extends Node2D
 
 
 
-func _ready() -> void:
-	#if we add a quit button to win/lin exports this will hide it in the future
-	#we just need to make sure its spelt "Quit"
-	if OS.has_feature("web"):
-		for i in ui_buttons:
-			if i.name == "Quit":
-				i.visible = false
-
-	#loop through and connect the press signal sending its name
-	for button in ui_buttons:
-		button.pressed.connect(_on_button_pressed.bind(button.name))
-
-
-func _on_button_pressed(action: StringName) -> void:
-	match action: #bases them off their names, we can change it later though!
-		"Start":
-			LevelManager.goto_shop()
-		"Settings":
-			pass
-
-
 func _on_reset_pressed() -> void:
 	SaveManager.pony_stats.reset_all_upgrades()
 	SaveManager.reset()
 	Global.reset()
 	EventChannel.reset_progress.emit()
+	LevelManager.goto_shop()
 
 
 func _on_tutorial_pressed() -> void:
 	tutorial.open()
+
+
+func _on_start_pressed() -> void:
+	LevelManager.goto_shop()
